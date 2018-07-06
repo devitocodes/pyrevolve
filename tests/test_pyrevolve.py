d5967b88487a5f1dfc5658306309e97bbda8d72d
Navjot Kukreja <navjotk@gmail.com>
2018-07-06 16:08:08 +0200

    Flake

--- tests/test_pyrevolve.py
+++ tests/test_pyrevolve.py
@@ -40,9 +40,10 @@ class SimpleOperator(Operator):
         t_start = kwargs['t_start']
         t_end = kwargs['t_end']
         self.counter += abs(t_end - t_start)
-    
 
-@pytest.mark.parametrize("nt, ncp", [(10, 2), (10, 4), (10, 6), (10, 8), (10, 9), (10, 10), (10, 11), (10, 12)])
+
+@pytest.mark.parametrize("nt, ncp", [(10, 2), (10, 4), (10, 6), (10, 8),
+                                     (10, 9), (10, 10), (10, 11), (10, 12)])
 def test_forward_nt(nt, ncp):
     cp = SimpleCheckpoint()
     f = SimpleOperator()
@@ -54,7 +55,9 @@ def test_forward_nt(nt, ncp):
 
     assert(f.counter == nt)
 
-@pytest.mark.parametrize("nt, ncp", [(10, 2), (10, 4), (10, 6), (10, 8), (10, 9), (10, 10), (10, 11), (10, 12)])
+
+@pytest.mark.parametrize("nt, ncp", [(10, 2), (10, 4), (10, 6), (10, 8),
+                                     (10, 9), (10, 10), (10, 11), (10, 12)])
 def test_reverse_nt(nt, ncp):
     cp = SimpleCheckpoint()
     f = SimpleOperator()
@@ -67,7 +70,9 @@ def test_reverse_nt(nt, ncp):
 
     assert(b.counter == nt)
 
-@pytest.mark.parametrize("nt, ncp", [(10, 2), (10, 4), (10, 6), (10, 8), (10, 9), (10, 10), (10, 11), (10, 12)])
+
+@pytest.mark.parametrize("nt, ncp", [(10, 2), (10, 4), (10, 6), (10, 8),
+                                     (10, 9), (10, 10), (10, 11), (10, 12)])
 def test_number_of_saves_in_forward(nt, ncp):
     cp = SimpleCheckpoint()
     f = SimpleOperator()
@@ -78,7 +83,9 @@ def test_number_of_saves_in_forward(nt, ncp):
     rev.apply_forward()
     assert(cp.save_counter == min(ncp, nt-1))
 
-@pytest.mark.parametrize("nt, ncp", [(10, 2), (10, 4), (10, 6), (10, 8), (10, 9), (10, 10), (10, 11), (10, 12)])
+
+@pytest.mark.parametrize("nt, ncp", [(10, 2), (10, 4), (10, 6), (10, 8),
+                                     (10, 9), (10, 10), (10, 11), (10, 12)])
 def test_num_loads_and_saves(nt, ncp):
     cp = SimpleCheckpoint()
     f = SimpleOperator()
@@ -92,7 +99,8 @@ def test_num_loads_and_saves(nt, ncp):
     assert(cp.load_counter >= cp.save_counter)
 
 
-@pytest.mark.parametrize("nt, ncp", [(10, 2), (10, 4), (10, 6), (10, 8), (10, 9), (10, 10), (10, 11), (10, 12)])
+@pytest.mark.parametrize("nt, ncp", [(10, 2), (10, 4), (10, 6), (10, 8),
+                                     (10, 9), (10, 10), (10, 11), (10, 12)])
 def test_ptr_loads_and_saves(nt, ncp):
     cp = SimpleCheckpoint()
     f = SimpleOperator()
@@ -101,6 +109,5 @@ def test_ptr_loads_and_saves(nt, ncp):
     rev = Revolver(cp, f, b, ncp, nt)
     rev.apply_forward()
     rev.apply_reverse()
-    assert(cp.save_pointers==cp.load_pointers)
+    assert(cp.save_pointers == cp.load_pointers)
     assert(len(cp.save_pointers) == min(ncp, nt - 1))
-    
