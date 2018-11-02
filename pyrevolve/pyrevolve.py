5dbc454d79c476095635a68730b42fac09d3e035
Navjot Kukreja <navjotk@gmail.com>
2018-11-02 15:57:38 -0400

    Add counts to profiling

--- pyrevolve/pyrevolve.py
+++ pyrevolve/pyrevolve.py
@@ -35,6 +35,7 @@ class Timer(object):
 class Profiler(object):
     def __init__(self):
         self.timings = {}
+        self.counts = {}
 
     def get_timer(self, section, action):
         return Timer(self, section, action)
@@ -45,12 +46,16 @@ class Profiler(object):
         section_timings[action] = section_timings.get(action, 0) + elapsed
         self.timings[section] = section_timings
 
+        section_counts = self.counts.get(section, {})
+        section_counts[action] = section_counts.get(action, 0) + 1
+        self.counts[section] = section_counts
+
     def summary(self):
         summary = '****************'
         for section, section_timings in self.timings.items():
             summary += '\nIn section %s:' % section
             for action, action_time in section_timings.items():
-                summary += '\n\tAction %s: %f' % (action, action_time)
+                summary += '\n\tAction %s: %f (%d)' % (action, action_time, self.counts[section][action])
         summary += '\n****************'
         return summary
 
