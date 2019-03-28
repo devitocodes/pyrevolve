b5fdcc4cd7d6dcf3866b2188b3348cf564bf8c34
navjotk <navjotk@gmail.com>
2019-03-28 10:37:52 +0000

    Update dependency to pyzfp

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 s_required = ["cython>=0.17", "versioneer"]
-i_required = ["blosc", "contexttimer", "pyzfp~=0.1.7rc1"]
+i_required = ["blosc", "contexttimer", "pyzfp~=0.2rc1"]
 
 configuration = {
     'name': 'pyrevolve',
