ee5765196968e94925bb5a897e1f0e0da64700dc
ALU <amorim.an@gmail.com>
2021-03-11 09:26:45 -0300

    Added Disk and MemoryRevolver classes; Added DiskStorage class; Added Disk and MemoryRevolver test cases to test_storage.py

--- pyrevolve/pyrevolve.py
+++ pyrevolve/pyrevolve.py
@@ -1,13 +1,11 @@
 from abc import ABCMeta, abstractproperty, abstractmethod
 
 import numpy as np
-
-
 from . import crevolve as cr
 from .compression import init_compression as init
 from .schedulers import Revolve, Action
 from .profiling import Profiler
-from .storage import NumpyStorage, BytesStorage
+from .storage import NumpyStorage, BytesStorage, DiskStorage
 
 
 class Operator(object):
@@ -45,11 +43,10 @@ class Checkpoint(object):
         return self.size * np.dtype(self.dtype).itemsize
 
 
-class Revolver(object):
+class BaseRevolver(object):
     """
     This should be the only user-facing class in here. It manages the
     interaction between the operators passed by the user, and the data storage.
-
     TODO:
         * Reverse operator is always called for a single step. Change this.
         * Avoid redundant data stores if higher-order stencils save multiple
@@ -59,10 +56,11 @@ class Revolver(object):
         * Find a better name than `checkpoint`, as the object with that name
           stores live data rather than one of the checkpoints.
     """
+    __metaclass__ = ABCMeta
 
     def __init__(self, checkpoint, fwd_operator, rev_operator,
                  n_checkpoints=None, n_timesteps=None, timings=None,
-                 compression_params=None):
+                 profiler=None):
         """Initialise checkpointer for a given forward- and reverse operator, a
         given number of time steps, and a given storage strategy. The number of
         time steps must currently be provided explicitly, and the storage must
@@ -71,32 +69,25 @@ class Revolver(object):
             raise Exception("Online checkpointing not yet supported. Specify \
                               number of time steps!")
         if(n_checkpoints is None):
-            n_checkpoints = cr.adjust(n_timesteps)
-        if compression_params is None:
-            compression_params = {'scheme': None}
+            self.n_checkpoints = cr.adjust(n_timesteps)
+        else:
+            self.n_checkpoints = n_checkpoints
+
+        if profiler is None:
+            self.profiler = Profiler()
+        else:
+            self.profiler = profiler
+
         self.timings = timings
         self.fwd_operator = fwd_operator
         self.rev_operator = rev_operator
         self.checkpoint = checkpoint
-        compressor, decompressor = init(compression_params)
-        self.profiler = Profiler()
-
-        if compression_params['scheme'] is None:
-            self.storage = NumpyStorage(checkpoint.size, n_checkpoints,
-                                        checkpoint.dtype,
-                                        profiler=self.profiler)
-        else:
-            self.storage = BytesStorage(checkpoint.nbytes, n_checkpoints,
-                                        checkpoint.dtype, auto_pickle=True,
-                                        compression=(compressor, decompressor))
         self.n_timesteps = n_timesteps
-
-        self.scheduler = Revolve(n_checkpoints, n_timesteps)
+        self.scheduler = Revolve(self.n_checkpoints, n_timesteps)
 
     def apply_forward(self):
         """Executes only the forward computation while storing checkpoints,
         then returns."""
-
         while(True):
             # ask Revolve what to do next.
             action = self.scheduler.next()
@@ -127,7 +118,6 @@ class Revolver(object):
         with self.profiler.get_timer('reverse', 'reverse'):
             self.rev_operator.apply(t_start=self.scheduler.capo,
                                     t_end=self.scheduler.capo+1)
-
         while(True):
             # ask Revolve what to do next.
             action = self.scheduler.next()
@@ -163,3 +153,71 @@ class Revolver(object):
     def load_checkpoint(self):
         locations = self.checkpoint.get_data_location(self.scheduler.capo)
         self.storage.load(self.scheduler.cp_pointer, locations)
+
+    @abstractmethod
+    def createStorage(self):
+        return NotImplemented
+
+
+class DiskRevolver(BaseRevolver):
+    """
+        This class is an specialization of the Revolver class
+        that uses a DiskStorage as its default storage method.
+        When no 'filename' is provided, the storage
+        .dat file is created inside the working directory.
+        The storage file is removed by default when storage
+        object is destroyed. DiskRevolver uses a single
+        checkpoint file by default.
+    """
+    def __init__(self, checkpoint, fwd_operator, rev_operator,
+                 n_checkpoints=None, n_timesteps=None, timings=None,
+                 filedir="./", singlefile=True):
+        super().__init__(checkpoint, fwd_operator, rev_operator, n_checkpoints,
+                         n_timesteps, timings)
+        self.filedir = filedir
+        self.singlefile = singlefile
+        self.createStorage()
+
+    def createStorage(self):
+        self.storage = DiskStorage(self.checkpoint.size, self.n_checkpoints,
+                                   self.checkpoint.dtype, self.profiler,
+                                   filedir=self.filedir,
+                                   singlefile=self.singlefile)
+
+
+class MemoryRevolver(BaseRevolver):
+    """
+        This class is an specialization of the Revolver class
+        that uses a NumpyStorage as its default storage method.
+    """
+    def __init__(self, checkpoint, fwd_operator, rev_operator,
+                 n_checkpoints=None, n_timesteps=None, timings=None,
+                 compression_params=None):
+        super().__init__(checkpoint,
+                         fwd_operator,
+                         rev_operator,
+                         n_checkpoints,
+                         n_timesteps,
+                         timings)
+
+        if compression_params is None:
+            compression_params = {'scheme': None}
+
+        self.compression_params = compression_params
+        self.createStorage()
+
+    def createStorage(self):
+        if self.compression_params['scheme'] is None:
+            self.storage = NumpyStorage(self.checkpoint.size, self.n_checkpoints,
+                                        self.checkpoint.dtype,
+                                        profiler=self.profiler)
+        else:
+            compressor, decompressor = init(self.compression_params)
+            self.storage = BytesStorage(self.checkpoint.nbytes, self.n_checkpoints,
+                                        self.checkpoint.dtype, auto_pickle=True,
+                                        compression=(compressor, decompressor))
+
+
+""" To keep backward compatibility with previous testcases
+and all previous codes that use the name Revolver """
+Revolver = MemoryRevolver
