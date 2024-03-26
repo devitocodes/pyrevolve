eb2f51fe0e1eec3089a06339865dfc29c9d9b5d1
mloubout <mathias.louboutin@gmail.com>
2024-03-26 09:40:36 -0400

    update versioneer

--- pyrevolve/__init__.py
+++ pyrevolve/__init__.py
@@ -3,6 +3,5 @@ from __future__ import absolute_import
 from pyrevolve.pyrevolve import * # noqa
 from pyrevolve.crevolve import * # noqa
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
